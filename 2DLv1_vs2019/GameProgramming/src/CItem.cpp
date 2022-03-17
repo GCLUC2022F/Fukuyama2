266e9b45ebba8bd295196794f6e21c52faa2ffdc
FukiharaYoshihiro <21010513@anabuki-net.ne.jp>
2022-03-17 16:43:06 +0900

    ib

--- 2DLv1_vs2019/GameProgramming/src/CItem.cpp
+++ 2DLv1_vs2019/GameProgramming/src/CItem.cpp
@@ -30,19 +30,26 @@ void CItem::Update()
 
 void CItem::Collision(CRectangle* i, CRectangle* y)
 {
-	if (i->mTag == EITEM) {
-		if (y->mTag == EPLAYER) {
-			(Itemno == 1 ? CPlayer::PWeapon = 1 :
-				Itemno == 2 ? CPlayer::PWeapon = 2 :
-				Itemno == 3 ? CPlayer::PLife += PLAYER_LIFE / 4 :
-				Itemno == 4 ? CPlayer::PLife += PLAYER_LIFE / 2 :
-				Itemno == 5 ? CPlayer::PLife += PLAYER_LIFE :
-				mEnabled = false);
+	if (i->mTag == EITEM && i->mEnabled == true) {
+		if (y->mTag == EPLAYER && y->mEnabled == true) {
+			int mx, my;
+			if (CRectangle::Collision(y, &mx, &my)) {
+				(Itemno == 1 ? CPlayer::PWeapon = 1 :
+					Itemno == 2 ? CPlayer::PWeapon = 2 :
+					Itemno == 3 ? CPlayer::PLife += PLAYER_LIFE / 4 :
+					Itemno == 4 ? CPlayer::PLife += PLAYER_LIFE / 2 :
+					Itemno == 5 ? CPlayer::PLife += PLAYER_LIFE :
+					mEnabled = false);
+				i->mEnabled = false;
+			}
 		}
+
 	}
 }
 
 void CItem::Render()
 {
-	//Itemno ‚É‚æ‚Á‚Ä•Ï‚¦‚é
+	if (mEnabled) {
+
+	}
 }
